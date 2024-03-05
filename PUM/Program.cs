51ec50afe7bcd4b03ced5bc867a0bfe0ab1c84e7
unknown <islammohamedahmed49@gmail.com>
2024-03-05 18:47:21 +0200

    PUM

---
+++ PUM/Program.cs
@@ -0,0 +1,8 @@
+﻿int N, x = 0;
+N = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+
+    Console.WriteLine((x + 1) + " " + (x + 2) + " " + (x + 3) + " " + "PUM");
+    x += 4;
+}
